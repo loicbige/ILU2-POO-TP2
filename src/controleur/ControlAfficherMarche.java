30b7af469537cfda9e188b4379ee113b7aec84ff
loicbige <loic.bige@univ-tlse3.fr>
2026-04-10 14:23:09 +0200

    final verison POO TP2

--- src/controleur/ControlAfficherMarche.java
+++ src/controleur/ControlAfficherMarche.java
@@ -9,5 +9,7 @@ public class ControlAfficherMarche {
 		this.village = village;
 	}
 
-	//TODO a completer
+	public String[] donnerInfoMarche() {
+		return village.donnerEtatMarche();
+	}
 }
--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,18 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infoMarche = controlAfficherMarche.donnerInfoMarche();
+		if (infoMarche.length == 0) {
+			System.out.println("Le marché est vide, revenez plus tard.");
+		} else {
+			System.out.println(nomAcheteur + ", vous trouverez au marché :");
+			for (int i = 0; i < infoMarche.length; i++) {
+				System.out.println("vendeur = " + infoMarche[i]);
+				i++;
+				System.out.println("Quantité = " + infoMarche[i]);
+				i++;
+				System.out.println("produit = " + infoMarche[i]);
+			}
+		}
 	}
 }
