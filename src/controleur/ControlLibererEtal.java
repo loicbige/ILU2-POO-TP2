7f4981aec29323e01f2b77c46f0eef770a8e45bc
loicbige <loic.bige@univ-tlse3.fr>
2026-04-10 13:50:23 +0200

    libererEtal working

--- src/controleur/ControlLibererEtal.java
+++ src/controleur/ControlLibererEtal.java
@@ -1,5 +1,7 @@
 package controleur;
 
+import villagegaulois.Etal;
+
 public class ControlLibererEtal {
 	private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
 
@@ -8,7 +10,9 @@ public class ControlLibererEtal {
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
 
-	//TODO a completer
+	public boolean isVendeur(String nomVendeur) {
+		return controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur) != null;
+	}
 
 	/**
 	 * 
@@ -21,8 +25,10 @@ public class ControlLibererEtal {
 	 * 		[4] : quantité de produit vendu
 	 */
 	public String[] libererEtal(String nomVendeur) {
-		//TODO a completer
-		String[] donneesEtal = null;
+		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+		
+		String[] donneesEtal = etal.etatEtal();
+		etal.libererEtal();
 		return donneesEtal;
 	}
 
--- src/frontiere/BoundaryLibererEtal.java
+++ src/frontiere/BoundaryLibererEtal.java
@@ -8,9 +8,21 @@ public class BoundaryLibererEtal {
 	public BoundaryLibererEtal(ControlLibererEtal controlLibererEtal) {
 		this.controlLibererEtal = controlLibererEtal;
 	}
-
+	
 	public void libererEtal(String nomVendeur) {
-		//TODO a completer
+		if (!controlLibererEtal.isVendeur(nomVendeur)) {
+			System.out.println("Mais vous n'êtes même pas inscrit sur notre marché aujoud'hui !");
+		} else {
+			String[] donneesEtal = controlLibererEtal.libererEtal(nomVendeur);
+			
+			String etalOccupe = donneesEtal[0];
+			if (etalOccupe.equals("true")) {
+				System.out.println("Vous avez vendu "+ donneesEtal[4] + " " +donneesEtal[2] + " sur "+donneesEtal[3]+".");
+				System.out.println("Au revoir "+ nomVendeur +", passez une bonne journée");
+			}
+		}
+		
+		
 	}
 
 }
